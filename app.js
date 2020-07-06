64746a517d518d3101cbb1d7bca652031b90197e
Leandro Martins <leosonic2@gmail.com>
2020-07-06 14:11:50 -0300

    "commit inicial do projeto"

---
+++ app.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import mongoose from 'mongoose';
+
+import { studentRouter } from './routes/studentRouter.js';
+
+//Conectar ao MongoDB pelo mongoose
+(async () => {
+  try {
+    await mongoose.connect(
+      'mongodb+srv://leosonic2:@Tencao24229@leandrocluster.1zl6y.mongodb.net/grades?retryWrites=true&w=majority',
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    );
+  } catch (error) {
+    console.log('Erro ao conectar ao mongoDB');
+  }
+})();
+
+const app = express();
+app.use(express.json());
+app.use(studentRouter);
+
+app.listen(3000, () => {
+  console.log('API iniciada');
+});
---
+++ models/student.js
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose';
+
+//Criação do modelo
+const studentSchema = mongoose.Schema({
+  name: { type: String, required: true },
+  subject: { type: String, required: true },
+  type: { type: String, required: true },
+  value: {
+    type: Number,
+    required: true,
+    validate(value) {
+      if (value < 0) {
+        throw new Error('Valor Negativo para a nota não permitido');
+      }
+    },
+  },
+  lastModified: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+//definindo Modelo da Coleção
+const studentModel = mongoose.model('student', studentSchema, 'student');
+
+export { studentModel };
